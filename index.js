4e5c2609116e6915eb3953beb633101ffd0ff591
Milos Djordjevic <milosdjor011@gmail.com>
2020-01-23 11:47:58 -0600

    test

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const layOut = require('./views/layout.js');
+const content = require('./views/layout.js')
+
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send(layOut(content));
+});
+const PORT = 1337 ;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
